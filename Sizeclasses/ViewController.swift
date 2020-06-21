2f43d191426ffbcea795c3dcaf18cdb0ef820aec
Manojkumar <manojkumar.jonna@wibmo.com>
2020-06-21 21:34:53 +0530

    cool

---
+++ Sizeclasses/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sizeclasses
+//
+//  Created by Manojkumar on 17/06/20.
+//  Copyright © 2020 Manojkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var customView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+       // print(self.customView.frame)
+    }
+
+
+}
+
