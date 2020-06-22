34465a31ece9991d8118b838719c3f445990de1f
abhirai25 <abhi.thefan@gmail.com>
2020-06-21 21:50:48 -0400

    push branch

--- Sizeclasses/AppDelegate.swift
+++ Sizeclasses/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("Hi from manoj")
         print("This is my first push")
         print("this is my first push through source tree")
+        print("push through branch")
         return true
     }
 
